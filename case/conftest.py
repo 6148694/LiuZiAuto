8d1ea420608ff85811cba5499489b1175910b083
6148694 <6148694@qq.com>
2020-08-19 10:33:16 +0800

    同步Linux代码

--- case/conftest.py
+++ case/conftest.py
@@ -12,28 +12,30 @@ import platform
 host_api_url = "https://sofia.ezijing.com"
 delete_api_url = "https://api.ezijing.com/zws/v1/enrollment/submissions/delete"
 login_api_url ="https://sofia.ezijing.com/api/passport/rest/login"
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def driver(request):
     """定义全局driver fixture，给其它地方作参数调用"""
     if platform.system() == "Windows":
         _driver = webdriver.Chrome()
         _driver.maximize_window()
         _driver.get(host_api_url)
+        print("目前是window")
     else:
         chrome_options = Options()
-        chrome_options.add_argument('--window-size=1920,1080')  # 设置当前窗口的宽度和高度
-        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错问题
-        chrome_options.add_argument('--disable-gpu')   # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动。
+        chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在报错
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')   # 禁用GPU硬件加速。如果软件渲染器没有就位，则GPU进程将不会启动
+        chrome_options.add_argument('--headless')
         _driver = webdriver.Chrome(chrome_options=chrome_options)
         _driver.get(host_api_url)
+        print("目前是Linux")
     def end():
         """测试用例完成以后，执行终结函数"""
         time.sleep(3)
         _driver.quit()
     request.addfinalizer(end)
     return _driver
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def _login(driver):
     """前置条件登录"""
     web = LoginPage(driver)
